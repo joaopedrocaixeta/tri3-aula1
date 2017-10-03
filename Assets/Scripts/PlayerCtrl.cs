32f7399e82b63c1565ea1a6ec484f4df3812279e
João Pedro Caixeta <j.p.a.caixeta@gmail.com>
2017-10-03 12:01:58 -0300

    Iniciando movimento de pulo

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerCtrl : MonoBehaviour {
 
 	public float horizontalSpeed = 10f;
+	public float jumpSpeed = 600f;
 
 	Rigidbody2D rb;
 
@@ -25,6 +26,10 @@ public class PlayerCtrl : MonoBehaviour {
 			StopMovingHorizontal ();
 		}
 
+		if (Input.GetButtonDown("Jump")) {
+			Jump();
+		}
+
 	}
 
 	void MoveHorizontal (float speed) {
@@ -33,5 +38,9 @@ public class PlayerCtrl : MonoBehaviour {
 
 	void StopMovingHorizontal () {
 		rb.velocity = new Vector2(0f, rb.velocity.y);
+	}
+
+	void Jump () {
+		rb.AddForce(new Vector2(0f, jumpSpeed));
 	}
 }
