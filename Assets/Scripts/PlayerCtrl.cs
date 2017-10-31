7863bb1f6f9f53491c537fd7d7190e642c5108b1
João Pedro Caixeta <j.p.a.caixeta@gmail.com>
2017-10-31 10:32:45 -0200

    Implementando pulo simples

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -22,13 +22,27 @@ public class PlayerCtrl : MonoBehaviour {
 
 	bool isJumping = false;
 
+	public Transform feet;
+
+	public float feetWidth = 0.5f;
+	public float feetHeight = 0.1f;
+
+	public bool isGrounded;
+	public LayerMask whatIsGround;
+
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
 		sr = GetComponent<SpriteRenderer>();
 		anim = GetComponent<Animator>();
 	}
 	
+	void OnDrawGizmos(){
+		Gizmos.DrawWireCube(feet.position, new Vector3(feetWidth, feetHeight, 0f));
+	}
+
 	void Update () {
+
+		isGrounded = Physics2D.OverlapBox(new Vector2(feet.position.x, feet.position.y), new Vector2(feetWidth, feetHeight), 360.0f, whatIsGround);
 		
 		float horizontalInput = Input.GetAxisRaw("Horizontal");
 		float horizontalPlayerSpeed = horizontalSpeed * horizontalInput;
@@ -76,9 +90,11 @@ public class PlayerCtrl : MonoBehaviour {
 	}
 
 	void Jump () {
-		isJumping = true;
-		rb.AddForce(new Vector2(0f, jumpSpeed));
-		anim.SetInteger("State", 1);
+		if (isGrounded){
+			isJumping = true;
+			rb.AddForce(new Vector2(0f, jumpSpeed));
+			anim.SetInteger("State", 1);
+		}
 	}
 
 	void OnCollisionEnter2D(Collision2D other){
